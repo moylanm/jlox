980a8525eb32b413a52a7cae29856f69bd9bcfbd
Myles Moylan <moylanm@fastmail.com>
2023-12-16 11:40:35 -0800

    Interpreting global variables

--- app/src/main/java/jlox/Interpreter.java
+++ app/src/main/java/jlox/Interpreter.java
@@ -3,7 +3,8 @@ package jlox;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-
+    private Environment environment = new Environment();
+    
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -32,6 +33,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
@@ -94,6 +100,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     public Void visitPrintStmt(Stmt.Print stmt) {
         Object value = evaluate(stmt.expression);
         System.err.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
         return null;
     }
 
